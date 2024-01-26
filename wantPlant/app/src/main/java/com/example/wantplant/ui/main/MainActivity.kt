162b6e2b9e643256bba5659a86ff9162cd6de199
2hyunjinn <huenjin04@naver.com>
2024-01-26 22:18:08 +0900

    Feat: 카카오 계정으로 로그인 api 연결

--- wantPlant/app/src/main/java/com/example/wantplant/ui/main/MainActivity.kt
+++ wantPlant/app/src/main/java/com/example/wantplant/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wantplant.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.wantplant.R
 import com.example.wantplant.databinding.ActivityMainBinding
 import com.example.wantplant.ui.main.book.BookFragment
@@ -10,6 +11,7 @@ import com.example.wantplant.ui.main.book.LandingPageFragment
 import com.example.wantplant.ui.main.garden.GardenFragment
 import com.example.wantplant.ui.main.profile.ProfileFragment
 import com.example.wantplant.ui.main.water.month.WaterMonthFragment
+import com.kakao.sdk.common.util.Utility
 
 class MainActivity : AppCompatActivity() {
 
--- wantPlant/app/src/main/java/com/example/wantplant/ui/main/water/week/WaterWeekFragment.kt
+++ wantPlant/app/src/main/java/com/example/wantplant/ui/main/water/week/WaterWeekFragment.kt
@@ -12,10 +12,10 @@ import com.example.wantplant.R
 import com.example.wantplant.databinding.FragmentWaterWeekBinding
 import com.example.wantplant.ui.main.MainActivity
 import com.example.wantplant.ui.main.water.month.WaterMonthFragment
-import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
-import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
-import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
-import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
+//import com.michalsvec.singlerowcalendar.calendar.CalendarChangesObserver
+//import com.michalsvec.singlerowcalendar.calendar.CalendarViewManager
+//import com.michalsvec.singlerowcalendar.calendar.SingleRowCalendarAdapter
+//import com.michalsvec.singlerowcalendar.selection.CalendarSelectionManager
 import java.util.Calendar
 import java.util.Date
 
@@ -32,7 +32,7 @@ class WaterWeekFragment : Fragment() {
     ): View? {
         binding = FragmentWaterWeekBinding.inflate(layoutInflater)
 
-        initCalendar()
+        // initCalendar()
 
         clickListener()
 
@@ -41,84 +41,86 @@ class WaterWeekFragment : Fragment() {
         return binding.root
     }
 
-    private fun initCalendar() {
-        calendar.time = Date()
-        currentMonth = calendar[Calendar.MONTH]
+    /*
+private fun initCalendar() {
+    calendar.time = Date()
+    currentMonth = calendar[Calendar.MONTH]
 
-        val myCalendarViewManager = object : CalendarViewManager {
-            override fun bindDataToCalendarView(
-                holder: SingleRowCalendarAdapter.CalendarViewHolder,
-                date: Date,
-                position: Int,
-                isSelected: Boolean
-            ) {
+    val myCalendarViewManager = object : CalendarViewManager {
+        override fun bindDataToCalendarView(
+            holder: SingleRowCalendarAdapter.CalendarViewHolder,
+            date: Date,
+            position: Int,
+            isSelected: Boolean
+        ) {
 
-                val dayOfMonth = com.michalsvec.singlerowcalendar.utils.DateUtils.getDayNumber(date)
-                val dayOfWeek = com.michalsvec.singlerowcalendar.utils.DateUtils.getDay3LettersName(date)
+            val dayOfMonth = com.michalsvec.singlerowcalendar.utils.DateUtils.getDayNumber(date)
+            val dayOfWeek = com.michalsvec.singlerowcalendar.utils.DateUtils.getDay3LettersName(date)
 
-                // Set the day of the week in item_day_tv
-                (holder.itemView.findViewById<View>(R.id.item_day_tv) as? TextView)?.text = dayOfWeek
+            // Set the day of the week in item_day_tv
+            (holder.itemView.findViewById<View>(R.id.item_day_tv) as? TextView)?.text = dayOfWeek
 
-                // Set the day of the month in item_date_tv
-                (holder.itemView.findViewById<View>(R.id.item_date_tv) as? TextView)?.text = dayOfMonth
-            }
-
-            override fun setCalendarViewResourceId(
-                position: Int,
-                date: Date,
-                isSelected: Boolean
-            ): Int {
-                val cal = Calendar.getInstance()
-                cal.time = date
-                return if (isSelected)
-                    R.layout.item_water_week_selected
-                else
-                    R.layout.item_water_week_deselected
-            }
+            // Set the day of the month in item_date_tv
+            (holder.itemView.findViewById<View>(R.id.item_date_tv) as? TextView)?.text = dayOfMonth
         }
 
-        val mySelectionManager = object : CalendarSelectionManager {
-            override fun canBeItemSelected(position: Int, date: Date): Boolean {
-                val cal = Calendar.getInstance()
-                cal.time = date
-                Log.d("WaterWeekFragment", "Selected Date: $date")
-                return true
-            }
-        }
-
-        val myCalendarChangesObserver = object : CalendarChangesObserver {
-            override fun whenWeekMonthYearChanged(weekNumber: String,monthNumber: String,monthName: String,year: String,date: Date) {
-                super.whenWeekMonthYearChanged(weekNumber, monthNumber, monthName, year, date)
-            }
-
-            override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
-                super.whenSelectionChanged(isSelected, position, date)
-            }
-
-            override fun whenCalendarScrolled(dx: Int, dy: Int) {
-                super.whenCalendarScrolled(dx, dy)
-            }
-
-            override fun whenSelectionRestored() {
-                super.whenSelectionRestored()
-            }
-
-            override fun whenSelectionRefreshed() {
-                super.whenSelectionRefreshed()
-            }
-        }
-
-        binding.waterWeekCalendar.apply {
-            calendarViewManager = myCalendarViewManager
-            calendarChangesObserver = myCalendarChangesObserver
-            calendarSelectionManager = mySelectionManager
-            pastDaysCount = 365
-            futureDaysCount = 365
-            includeCurrentDate = true
-            init()
-
+        override fun setCalendarViewResourceId(
+            position: Int,
+            date: Date,
+            isSelected: Boolean
+        ): Int {
+            val cal = Calendar.getInstance()
+            cal.time = date
+            return if (isSelected)
+                R.layout.item_water_week_selected
+            else
+                R.layout.item_water_week_deselected
         }
     }
+
+    val mySelectionManager = object : CalendarSelectionManager {
+        override fun canBeItemSelected(position: Int, date: Date): Boolean {
+            val cal = Calendar.getInstance()
+            cal.time = date
+            Log.d("WaterWeekFragment", "Selected Date: $date")
+            return true
+        }
+    }
+
+    val myCalendarChangesObserver = object : CalendarChangesObserver {
+        override fun whenWeekMonthYearChanged(weekNumber: String,monthNumber: String,monthName: String,year: String,date: Date) {
+            super.whenWeekMonthYearChanged(weekNumber, monthNumber, monthName, year, date)
+        }
+
+        override fun whenSelectionChanged(isSelected: Boolean, position: Int, date: Date) {
+            super.whenSelectionChanged(isSelected, position, date)
+        }
+
+        override fun whenCalendarScrolled(dx: Int, dy: Int) {
+            super.whenCalendarScrolled(dx, dy)
+        }
+
+        override fun whenSelectionRestored() {
+            super.whenSelectionRestored()
+        }
+
+        override fun whenSelectionRefreshed() {
+            super.whenSelectionRefreshed()
+        }
+    }
+
+    binding.waterWeekCalendar.apply {
+        calendarViewManager = myCalendarViewManager
+        calendarChangesObserver = myCalendarChangesObserver
+        calendarSelectionManager = mySelectionManager
+        pastDaysCount = 365
+        futureDaysCount = 365
+        includeCurrentDate = true
+        init()
+    }
+}
+*/
+
 
     private fun clickListener() {
         binding.waterWeekChangeCalendarLl.setOnClickListener {
