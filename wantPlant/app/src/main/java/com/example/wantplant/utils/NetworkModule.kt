d828f0decf42b906fcb787cc9b9548a62979ec12
Hyejeong <myejeong0211@gmail.com>
2024-01-27 23:27:18 +0900

    Feat: api 연동 준비

--- wantPlant/app/src/main/java/com/example/wantplant/utils/NetworkModule.kt
+++ wantPlant/app/src/main/java/com/example/wantplant/utils/NetworkModule.kt
@@ -3,7 +3,7 @@ package com.example.wantplant.utils
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val BASE_URL = "https://edu-api-test.softsquared.com" // /를 붙이게 되면 interface부분에서 /를 제외하고 적어줘야함
+const val BASE_URL = "http://ec2-3-34-198-148.ap-northeast-2.compute.amazonaws.com:8080" // /를 붙이게 되면 interface부분에서 /를 제외하고 적어줘야함
 
 fun getRetrofit(): Retrofit {
     val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
