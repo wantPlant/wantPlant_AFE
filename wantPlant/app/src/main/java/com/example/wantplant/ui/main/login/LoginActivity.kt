3f962120fecb7b0c6dbd03e07a4e4a90cfd0f787
2hyunjinn <huenjin04@naver.com>
2024-01-29 17:59:45 +0900

    Feat: 카카오 계정으로 로그인 api 연결 4

--- wantPlant/app/src/main/java/com/example/wantplant/ui/main/login/LoginActivity.kt
+++ wantPlant/app/src/main/java/com/example/wantplant/ui/main/login/LoginActivity.kt
@@ -20,6 +20,7 @@ import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 
 class LoginActivity : AppCompatActivity() {
@@ -33,67 +34,44 @@ class LoginActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
+        var keyHash = Utility.getKeyHash(this)
+        Log.d("해시", keyHash)
+
         // 카카오톡 로그인 버튼 누를 때
         binding.loginStartBtn.setOnClickListener {
-            moveToMainActivity()
-//            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-//                if (error != null) {
-//                    when {
-//                        error.toString() == AuthErrorCause.AccessDenied.toString() -> {
-//                            Log.d("[카카오로그인]", "접근이 거부 됨(동의 취소)")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.InvalidClient.toString() -> {
-//                            Log.d("[카카오로그인]", "유효하지 않은 앱")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
-//                            Log.d("[카카오로그인]", "인증 수단이 유효하지 않아 인증할 수 없는 상태")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
-//                            Log.d("[카카오로그인]", "요청 파라미터 오류")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.InvalidScope.toString() -> {
-//                            Log.d("[카카오로그인]", "유효하지 않은 scope ID")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.Misconfigured.toString() -> {
-//                            Log.d("[카카오로그인]", "설정이 올바르지 않음(android key hash)")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.ServerError.toString() -> {
-//                            Log.d("[카카오로그인]", "서버 내부 에러")
-//                        }
-//
-//                        error.toString() == AuthErrorCause.Unauthorized.toString() -> {
-//                            Log.d("[카카오로그인]", "앱이 요청 권한이 없음")
-//                        }
-//
-//                        else -> { // Unknown
-//                            Log.d("[카카오로그인]", "기타 에러")
-//                        }
-//                    }
-//                } else if (token != null) {
-//                    Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
-////                            UserApiClient.instance.me { user, error ->
-////                                nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
-////                            }
-//                    moveToMainActivity()
-//                } else {
-//                    Log.d("카카오로그인", "토큰==null error==null")
-//                }
-//            }
-//
-//            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
-//                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
-//            } else {
-//                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
-//            }
+            val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+                if (error != null) {
+                    Log.e("LOGIN", "카카오계정으로 로그인 실패", error)
+                } else if (token != null) {
+                    Log.i("LOGIN", "카카오계정으로 로그인 성공 ${token.accessToken}")
+                    moveToMainActivity()
+                }
+            }
 
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
+                    if (error != null) {
+                        Log.e("LOGIN", "카카오톡으로 로그인 실패", error)
 
-            setContentView(binding.root)
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+                    } else if (token != null) {
+                        Log.i("LOGIN", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                        finish()
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
+            }
         }
     }
 
