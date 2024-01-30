1b2d2cbeac3cdec329ec5ac1dee9a0de9d6380cf
Hyejeong <myejeong0211@gmail.com>
2024-01-30 16:29:21 +0900

    Feat: 월간 달력 태그 POST api 연동

--- wantPlant/app/src/main/java/com/example/wantplant/data/remote/tag/TagRetrofitInterfaces.kt
+++ wantPlant/app/src/main/java/com/example/wantplant/data/remote/tag/TagRetrofitInterfaces.kt
@@ -4,9 +4,10 @@ import com.example.wantplant.data.remote.tag.request.TagPostRequest
 import com.example.wantplant.data.remote.tag.response.TagPostResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface TagRetrofitInterfaces {
-    @POST("/tag/add")
+    @POST("/api/tag/add")
     fun postTag(@Body tagAdd: TagPostRequest): Call<TagPostResponse>
 }
